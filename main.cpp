90faa0fc29638d3d0a5e2faf952942b50d6647a8
Jonathan Perdomo <perdomojonathan5@gmail.com>
2017-06-08 01:41:11 -0400

    Updated QWindow position to be offset from OpenCV window.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 #include "mainwindow.h"
 #include <QApplication>
 
+#include <QDesktopWidget>
+#include <QPoint>
+#include <QRect>
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    QRect r = w.geometry();
+    QPoint desktop_center = QApplication::desktop()->availableGeometry().center();
+    desktop_center.setX(desktop_center.x()*.5);
+    //desktop_center.setY(desktop_center.y()*1.75);
+    r.moveCenter(desktop_center);
+    w.setGeometry(r);
     w.show();
 
     return a.exec();
